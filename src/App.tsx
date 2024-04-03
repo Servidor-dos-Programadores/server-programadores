8cc5bee22ca3d56b171e697a3ad4e804e8478e54
Eduardo Dias <eduardoo.js19@gmail.com>
2024-04-03 01:10:36 -0300

    vercel deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { HowItWorks } from "./components/HowItWorks";
-import { Navbar } from "./components/Navbar";
+//import { Navbar } from "./components/Navbar";
 //import { Newsletter } from "./components/Newsletter";
 //import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
